b005811d43f93415d93c78599f1583b31b496b0e
Norbert Orzechowicz <norbert@orzechowicz.pl>
2018-11-09 21:55:02 +0100

    Replaced expectjs with assertjs

--- nodejs/common/spec/NoGame/Common/MoveSpeedSpec.js
+++ nodejs/common/spec/NoGame/Common/MoveSpeedSpec.js
@@ -1,14 +1,14 @@
 describe("Move Speed Calculator", () => {
-    const expect = require('expect.js');
+    const Assert = require('assert-js');
     const MoveSpeed = require('./../../../src/NoGame/Common/MoveSpeed');
 
     it("calculates move speed without modifiers", () => {
-        expect(MoveSpeed.calculateMoveTime(1)).to.be(500);
+        Assert.equal(MoveSpeed.calculateMoveTime(1),500);
     });
 
     it("calculates move speed with modifier", () => {
-        expect(MoveSpeed.calculateMoveTime(1, 100)).to.be(499);
-        expect(MoveSpeed.calculateMoveTime(1, 1000)).to.be(490);
-        expect(MoveSpeed.calculateMoveTime(1, 5000)).to.be(450);
+        Assert.equal(MoveSpeed.calculateMoveTime(1, 100), 499);
+        Assert.equal(MoveSpeed.calculateMoveTime(1, 1000), 490);
+        Assert.equal(MoveSpeed.calculateMoveTime(1, 5000) ,450);
     });
 });
