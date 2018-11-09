6740b676f554230548c8693bf441e2be5fb56936
Norbert Orzechowicz <norbert@orzechowicz.pl>
2018-11-09 22:09:57 +0100

    Updated assert-js version and replaced expect.js with it

--- nodejs/client/spec/NoGame/Client/Gfx/Sprite/PositionSpec.js
+++ nodejs/client/spec/NoGame/Client/Gfx/Sprite/PositionSpec.js
@@ -1,16 +1,31 @@
-import expect from 'expect.js';
+import Assert from 'assert-js';
 import Position from './../../../../../src/NoGame/Client/Gfx/Sprite/Position';
 
 describe("Gfx Size", () => {
 
     it("throws error when created from invalid values", () => {
-        expect(() => {new Position(-1, 10)}).to.throwError('Expected value -1 to be greater than 0');
-        expect(() => {new Position(0, 10)}).to.throwError('Expected value 0 to be greater than 0');
+        try {
+            new Position(-1, 10);
+        } catch (e) {
+            Assert.equal(e, 'Expected value -1 to be greater than 0');
+        }
 
-        expect(() => {new Position(10, -1)}).to.throwError('Expected value -1 to be greater than 0');
-        expect(() => {new Position(10, 0)}).to.throwError('Expected value 0 to be greater than 0');
+        try {
+            new new Position(0, 10);
+        } catch (e) {
+            Assert.equal(e, 'Expected value 0 to be greater than 0');
+        }
 
-        expect(() => {new Position("string", 0)}).to.throwError('Expected number but got "string["string"]".');
-        expect(() => {new Position(10, "string")}).to.throwError('Expected number but got "string["string"]".');
+        try {
+            new Position(10, -1)
+        } catch (e) {
+            Assert.equal(e, 'Expected value -1 to be greater than 0');
+        }
+
+        try {
+            new Position(10, 0)
+        } catch (e) {
+            Assert.equal(e, 'Expected value 0 to be greater than 0');
+        }
     });
 });
--- nodejs/client/spec/NoGame/Client/Network/MessagesSpec.js
+++ nodejs/client/spec/NoGame/Client/Network/MessagesSpec.js
@@ -1,24 +1,24 @@
-import expect from 'expect.js';
 import LoginMessage from './../../../../src/NoGame/Client/Network/LoginMessage';
 import MoveMessage from './../../../../src/NoGame/Client/Network/MoveMessage';
 import SayMessage from './../../../../src/NoGame/Client/Network/SayMessage';
+import Assert from "assert-js";
 
 describe("Messages", () => {
     it("allows to stringify login message", () => {
         let message = new LoginMessage("norbert", "password");
 
-        expect(message.toString()).to.be('{"index":0,"name":"login","data":{"login":"norbert","password":"password"}}');
+        Assert.equal(message.toString(), '{"index":0,"name":"login","data":{"login":"norbert","password":"password"}}');
     });
 
     it("allows to stringify move message", () => {
         let message = new MoveMessage(10, 10);
 
-        expect(message.toString()).to.be('{"index":0,"name":"move","data":{"x":10,"y":10}}');
+        Assert.equal(message.toString(), '{"index":0,"name":"move","data":{"x":10,"y":10}}');
     });
 
     it("allows to stringify say message", () => {
         let message = new SayMessage("Test Message");
 
-        expect(message.toString()).to.be('{"index":0,"name":"message","data":{"message":"Test Message"}}');
+        Assert.equal(message.toString(), '{"index":0,"name":"message","data":{"message":"Test Message"}}');
     });
 });
